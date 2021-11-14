730b119706228a2401ffa85d75d8740d6ad75661
ignathiooo <68048278+ignathiooo@users.noreply.github.com>
2021-11-14 20:47:45 +0300

    Add files via upload

---
+++ exercise3.js
@@ -0,0 +1,15 @@
+for (let i= 0; i <= 100; i++){
+    if (i % 3 !== 0)
+    console.log(i);
+    else if (i % 3 === 0)
+    console.log("Buzz"); 
+    else if (i % 15 === 0)
+    console.log("FizzBuzz");
+}
+
+for (let i=-50; i <=50; i++){
+    if (i % 3 !== 0)
+    console.log(i);
+    else if (i % 3 === 0)
+    console.log("Buzz");
+}
