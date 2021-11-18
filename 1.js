9ff6d2c4900e27aab26633a3b0a775a497b8bc8e
ignathiooo <68048278+ignathiooo@users.noreply.github.com>
2021-11-18 19:47:46 +0300

    Add files via upload
    DoIt Homework

---
+++ 1.js
@@ -0,0 +1,8 @@
+const participants = [
+    {name: 'Abu', idNumber: 'S7282395H', gender: 'female'},
+    {name: 'Mary', idNumber: 'T4689018Z', gender: 'male'},
+    {name: 'Suzi', idNumber: 'G5512873T', gender: 'male'},
+    {name: 'T Chakra', idNumber: 'T8198514B', gender: 'female'},
+];
+
+console.log(participants.idNumber);
