c7bafacff3a05e068ce19d5207e7087c966781b2
ignathiooo <68048278+ignathiooo@users.noreply.github.com>
2021-11-18 20:02:59 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -2,7 +2,11 @@ const participants = [
     {name: 'Abu', idNumber: 'S7282395H', gender: 'female'},
     {name: 'Mary', idNumber: 'T4689018Z', gender: 'male'},
     {name: 'Suzi', idNumber: 'G5512873T', gender: 'male'},
-    {name: 'T Chakra', idNumber: 'T8198514B', gender: 'female'},
-];
+    {name: 'T Chakra', idNumber: 'T8198514B', gender: 'female'}];
 
-console.log(participants.idNumber);
+const output = participants.map(function(idNumbers) {
+    return idNumbers.idNumber;
+})
+
+console.log(output)
+
