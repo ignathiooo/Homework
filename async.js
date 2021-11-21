261224ebfd627bb2d662c5a1249d98dc7d830179
ignathiooo <68048278+ignathiooo@users.noreply.github.com>
2021-11-21 09:06:53 +0300

    Add files via upload

---
+++ async.js
@@ -0,0 +1,11 @@
+console.log('Start')
+setTimeout(() => {
+    console.log('1 sec passed')
+},1000);
+setTimeout(() => {
+    console.log('Another 2 sec passed')
+},4000);
+setTimeout(() => {
+    console.log('End')
+},6000);
+
