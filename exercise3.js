8e8aeb39a189fae59db1268c92e7a1a40e6c107e
ignathiooo <68048278+ignathiooo@users.noreply.github.com>
2021-11-22 07:22:53 +0300

    Update exercise3.js

--- exercise3.js
+++ exercise3.js
@@ -1,14 +1,17 @@
 for (let i= 0; i <= 100; i++){
-    if (i % 3 !== 0)
-    console.log(i);
-    else if (i % 3 === 0)
-    console.log("Buzz"); 
-    else if (i % 15 === 0)
+    if (i % 3 === 0 && i % 5 === 0);
     console.log("FizzBuzz");
+    else if (i % 3 !== 0)
+    console.log(i);
+    else if (i % 5 === 0)
+    console.log("Buzz"); 
+    
 }
 
 for (let i=-50; i <=50; i++){
-    if (i % 3 !== 0)
+    if (i % 3 === 0 && i % 5 === 0);
+    console.log("FizzBuzz");
+    else if (i % 3 !== 0)
     console.log(i);
     else if (i % 3 === 0)
     console.log("Buzz");
